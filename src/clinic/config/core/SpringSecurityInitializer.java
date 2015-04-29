d6380a5081f9604f810a7bfa5dbe2b6f50cb76fd
Lukasz <lukasz.kochanek1991@gmail.com>
2015-04-29 23:48:07 +0200

    good configuration hibernate

--- src/clinic/config/core/SpringSecurityInitializer.java
+++ src/clinic/config/core/SpringSecurityInitializer.java
@@ -2,6 +2,7 @@ package clinic.config.core;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+
 public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
